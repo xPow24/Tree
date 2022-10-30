b2a68f998e517ce23fee5f192f7443df5942aa26
xPow24 <114853668+xPow24@users.noreply.github.com>
2022-10-30 08:57:55 +0700

    Create FenwickTree.cpp

---
+++ FenwickTree.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int MAXN = 2*1e5 + 1;
+int n, bit[MAXN];
+void update(int x) {
+  while (x) {
+    bit[x]++;
+    x -= x & -x;
+  }
+}
+int get(int x) {
+  int res = 0;
+  while (x <= n) {
+    res += bit[x];
+    x += x & -x;
+  }
+  return res;
+}
+int main() {
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    long long ans = 0;
+    int x;
+    cin >> n;
+    memset(bit, 0, sizeof(bit));
+    for (int i = 0; i < n; ++i) {
+      cin >> x;
+      ans += get(x); 
+      update(x);
+    }
+    cout << ans << '\n';
+  }
+}
