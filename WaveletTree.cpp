9d8b0965951e7af44752732e85a4c34d7ffb27eb
Lê Trọng Bảo <114853668+xPow24@users.noreply.github.com>
2022-10-31 20:13:14 +0700

    Update WaveletTree.cpp

--- WaveletTree.cpp
+++ WaveletTree.cpp
@@ -9,34 +9,34 @@ struct WVTree {
   WVTree *l, *r;
   vector<int> b;
   WVTree(int *from, int *to, int x, int y) {
-    lo = x, hi = y;
-    if (lo == hi || from >= to) return;
-    int mid = (lo + hi)/2;
-    auto f = [mid](int tt) {
-      return tt <= mid;
-    };
+    lo = x; hi = y;
+    if (from > to || lo == hi) return;
     b.reserve(to - from + 1);
     b.push_back(0);
-    for (auto it = from; it != to; it++) {
-      b.push_back(b.back() + f(*it));
+    int mid = (lo + hi)/2;
+    auto check = [mid](int tt) {
+      return tt <= mid; 
+    };
+    for (auto it = from; it != to; ++it) {
+      b.push_back(b.back() + check(*it));
     }
-    auto pivot = stable_partition(from, to, f);
+    auto pivot = stable_partition(from, to, check);
     l = new WVTree(from, pivot, lo, mid);
     r = new WVTree(pivot, to, mid + 1, hi);
   }
   int kth(int u, int v, int k) {
     if (u > v) return 0;
     if (lo == hi) return lo;
-    int inleft = b[v] - b[u - 1];
     int lb = b[u - 1], rb = b[v];
-    if (k <= inleft) return this->l->kth(lb + 1, rb, k);
-    return this->r->kth(u - lb, v - rb, k - inleft);
+    int cur = rb - lb;
+    if (k <= cur) return this->l->kth(lb + 1, rb, k);
+    else return this->r->kth(u - lb, v - rb, k - cur);
   }
   int LTE(int u, int v, int k) {
     if (u > v || k < lo) return 0;
     if (hi <= k) return v - u + 1;
     int lb = b[u - 1], rb = b[v];
-    return this->l->LTE(lb + 1, rb, k) + this->r->LTE(u - lb, v - rb, k);
+    return this->l->LTE(lb + 1, rb, k) + this->r->LTE(u - lb, v - rb, k); 
   }
   int count(int u, int v, int k) {
     if (u > v || k < lo || k > hi) return 0;
@@ -44,20 +44,21 @@ struct WVTree {
     int lb = b[u - 1], rb = b[v], mid = (lo + hi)/2;
     if (k <= mid) return this->l->count(lb + 1, rb, k);
     else return this->r->count(u - lb, v - rb, k);
-  }	
+  } 
 };
 
 int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL); cout.tie(NULL);
-  int q, n, k, l, r, maxn = 0;
+  int q, n, k, l, r, maxn = 0, minn = 1e9;
   cin >> n;
   for (int i = 1; i <= n; ++i) {
     cin >> a[i];
     maxn = max(maxn, a[i]);
+    minn = min(minn, a[i]);
   }
-  WVTree T(a + 1, a + n + 1, 1, maxn);
+  WVTree T(a + 1, a + n + 1, minn, maxn);
   cin >> q;
   while (q--) {
     int type;
