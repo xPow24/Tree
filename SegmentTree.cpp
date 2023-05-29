54e14c13b549066d8b7c2693a9b5d878db5746d9
Lê Trọng Bảo <114853668+xPow24@users.noreply.github.com>
2023-05-29 10:17:46 +0700

    Update SegmentTree.cpp

--- SegmentTree.cpp
+++ SegmentTree.cpp
@@ -4,27 +4,36 @@ const int MAXN = 1000, INF = 1000000009;
 int tree[MAXN*4], a[MAXN];
 
 void buildtree(int id, int l, int r) {
-  if (l == r) {tree[id] = a[l]; return;}
-  int mid = (l + r)/2;
-  buildtree(id*2, l, mid); buildtree(id*2 + 1, mid + 1, r);
-  tree[id] = max(tree[id*2], tree[id*2 + 1]);
+  if (l == r) { tree[id].val = a[l]; return; }
+  int mid = (l + r) >> 1;
+  buildtree(id * 2, l, mid); buildtree(id * 2 + 1, mid + 1, r);
+  tree[id].val = max(tree[id * 2].val, tree[id * 2 + 1].val);
 }
+
+void down(int id) {
+  int cur = tree[id].lazy;
+  tree[id * 2].val += cur; tree[id * 2].lazy += cur;
+  tree[id * 2 + 1].val += cur; tree[id * 2 + 1].lazy += cur;
+  tree[id].lazy = 0;
+}
+
+void upd(int id, int l, int r, int u, int v, int k) {
+  if (v < l || r < u) return;
+  if (u <= l && r <= v) {
+    tree[id].maxn += k;
+    tree[id].val += k;
+    return;
+  }
+  down(id);
+  int mid = (l + r) >> 1;
+  upd(id * 2, l, mid, u, v, k); upd(id * 2 + 1, mid + 1, r, u, v, k);
+  tree[id].val = max(tree[id * 2].val, tree[id * 2 + 1].val); 
+}
+
 int get_query(int id, int l, int r, int u, int v) {
   if (v < l || r < u) return -INF;
-  if (l <= u && v <= r) return tree[id];
-  int mid = (l + r)/2;
-  return max(get_query(id*2, l, mid + 1, u, v), get_query(id*2 + 1, mid + 1, r, u, v)); 
-}
-int main()
-{
-  int n, queries; cin >> n >> queries;
-  for (int i = 1; i <= n; ++i) {
-    cin >> a[i];
-  }
-  buildtree(1, 1, n);
-  while (queries--) {
-    int u, v;
-    cin >> u >> v;
-    cout << get_query(1, 1, n, u, v) << '\n';
-  }
+  if (l <= u && v <= r) return tree[id].val;
+  down(id);
+  int mid = (l + r) >> 1;
+  return max(get_query(id * 2, l, mid + 1, u, v), get_query(id * 2 + 1, mid + 1, r, u, v)); 
 }
