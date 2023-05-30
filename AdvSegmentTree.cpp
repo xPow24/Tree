e545adcccfb4eb2e0824cd9472f18f2797d94f95
Lê Trọng Bảo <114853668+xPow24@users.noreply.github.com>
2023-05-30 20:29:06 +0700

    Update AdvSegmentTree.cpp

--- AdvSegmentTree.cpp
+++ AdvSegmentTree.cpp
@@ -1,5 +1,6 @@
-// Chi dung voi truong hop N = 2 ^ K
+// Chi dung voi truong hop N = 2 ^ K va bai toan khong lien quan den hai dau mut
 // Cac ham o trong day duoc tinh theo range [l, r) hay [l, r - 1]
+
 
 const int MAXN = 3000;
 int n, h, t[(MAXN << 1) | 1], d[MAXN];
