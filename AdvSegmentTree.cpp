19290e765026e3a68747912ec642eceb7591cc28
Lê Trọng Bảo <114853668+xPow24@users.noreply.github.com>
2023-05-30 20:27:00 +0700

    Update AdvSegmentTree.cpp

--- AdvSegmentTree.cpp
+++ AdvSegmentTree.cpp
@@ -6,42 +6,45 @@ void build_segtree() {
 }
 
 void calc(int p, int k) {
-  if (d[p] == 0) t[p] = t[p << 1] + t[(p << 1) | 1];
-  else t[p] = d[p] * k;
+  if (d[p]) t[p] = d[p] * k;
+  else t[p] = t[p << 1] + t[(p << 1) | 1];
 }
 
-void apply(int p, int value, int k) {
-  t[p] = value * k;
-  if (p < n) d[p] = value;
+void apply(int p, int val, int k) {
+  t[p] = val * k;
+  if (p < n) d[p] = val;
 }
 
 void build(int l, int r) {
   int k = 2;
   for (l += n, r += n - 1; l > 1; k <<= 1) {
     l >>= 1; r >>= 1;
-    for (int i = r; i >= l; --i) calc(i, k);
+    for (int i = l; i <= r; ++i) calc(i, k);
   }
-}
+} 
 
 void push(int l, int r) {
   int s = h, k = 1 << (h - 1);
-  for (l += n, r += n - 1; s > 0; --s, k >>= 1)
-    for (int i = l >> s; i <= r >> s; ++i) if (d[i] != 0) {
-      apply(i << 1, d[i], k);
-      apply((i << 1) | 1, d[i], k);
-      d[i] = 0;
+  for (l += n, r += n - 1; s > 0; k >>= 1, --s) {
+    for (int i = l >> s; i <= r >> s; ++i) {
+      if (d[i]) {
+        apply(i << 1, d[i], k);
+        apply((i << 1) | 1, d[i], k);
+        d[i] = 0;
+      }
     }
+  }
 }
 
-void modify(int l, int r, int value) {
-  if (value == 0) return;
-  int l0 = l, r0 = r, k = 1;
+void modify(int l, int r, int val) {
+  if (!val) return;
+  int left_end = l, right_end = r, k = 1;
   for (l += n, r += n; l < r; l >>= 1, r >>= 1, k <<= 1) {
-    if (l & 1) apply(l++, value, k);
-    if (r & 1) apply(--r, value, k);
+    if (l & 1) apply(l++, val, k);
+    if (r & 1) apply(--r, val, k);
   }
-  build(l0, l0 + 1);
-  build(r0 - 1, r0);
+  build(left_end, left_end + 1);
+  build(right_end - 1, right_end);
 }
 
 int query(int l, int r) {
