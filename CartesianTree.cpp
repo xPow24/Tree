8fc2d5f341cfee732f97b398114ab3c0c714abb1
Lê Trọng Bảo <114853668+xPow24@users.noreply.github.com>
2023-07-03 17:44:41 +0700

    Update CartesianTree.cpp

--- CartesianTree.cpp
+++ CartesianTree.cpp
@@ -1,13 +1,19 @@
-int N; cin >> N;
-vector<int> a(N + 1), sta(N + 1);
-vector<vector<int>> ct(N + 1, vector<int>(2, 0));
-int top = 0, K = 0;
-for (int i = 1; i <= N; ++i) {
-  cin >> a[i];
-  while (top && a[sta[top]] < a[i]) {
-    ct[i][0] = sta[top];
-    top--;
+const int MAXN = 1e5;
+int N, a[MAXN], sta[MAXN];
+int ct[MAXN][2];
+
+// ct[u][0] left child node of u
+// ct[u][1] right child node of u
+
+void build() {
+  int top = 0, K = 0;
+  for (int i = 1; i <= N; ++i) {
+    cin >> a[i];
+    while (top && a[sta[top]] < a[i]) {
+      ct[i][0] = sta[top];
+      top--;
+    }
+    if (top) ct[sta[top]][1] = i;
+    sta[++top] = i;
   }
-  if (top) ct[sta[top]][1] = i;
-  sta[++top] = i;
 }
