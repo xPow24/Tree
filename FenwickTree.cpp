3ec87d20a2306eb6c9e6160395bc9b572e9aee7d
Lê Trọng Bảo <114853668+xPow24@users.noreply.github.com>
2023-07-03 17:48:06 +0700

    Update FenwickTree.cpp

--- FenwickTree.cpp
+++ FenwickTree.cpp
@@ -1,6 +1,4 @@
-#include<bits/stdc++.h>
-using namespace std;
-const int MAXN = 2*1e5 + 1;
+const int MAXN = 2 * 1e5 + 1;
 int n, bit[MAXN];
 void update(int x) {
   while (x) {
@@ -8,27 +6,11 @@ void update(int x) {
     x -= x & -x;
   }
 }
-long long get(int x) {
-  long long res = 0;
+int get(int x) {
+  int res = 0;
   while (x <= n) {
     res += bit[x];
     x += x & -x;
   }
   return res;
-}
-int main() {
-  int tc;
-  cin >> tc;
-  while (tc--) {
-    long long ans = 0;
-    int x;
-    cin >> n;
-    memset(bit, 0, sizeof(bit));
-    for (int i = 0; i < n; ++i) {
-      cin >> x;
-      ans += get(x); 
-      update(x);
-    }
-    cout << ans << '\n';
-  }
 }
